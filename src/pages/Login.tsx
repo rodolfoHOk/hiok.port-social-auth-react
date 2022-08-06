21e66422fd69a97a46b6d6ef4a80ddb8a165d6a7
Rodolfo H Okino <rodolfo.okino@gmail.com>
2022-08-05 23:19:14 -0300

    add loading spinner in login buttons

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import facebookLogo from '../assets/images/facebook-logo.png';
 import githubLogo from '../assets/images/github-logo.png';
 import googleLogo from '../assets/images/google-logo.png';
 import { Provider, useAuth } from '../context/AuthenticationContext';
+import { SpinnerGap } from 'phosphor-react';
 
 export function Login() {
   const { isLoggingIn, login } = useAuth();
@@ -12,12 +13,14 @@ export function Login() {
       <div className="flex w-1/2">
         <Home />
       </div>
+
       <div className="flex flex-col items-center justify-center gap-6 w-1/2 bg-violet-400 dark:bg-purple-900">
         <div className="flex">
           <h2 className="font-Capriola text-zinc-900 dark:text-zinc-300">
             Logar no Portfólio de HiOk Dev
           </h2>
         </div>
+
         <div className="flex flex-col gap-4">
           <button
             className="flex justify-start items-center gap-1 h-10 bg-slate-50 dark:bg-slate-200 rounded-[4px] overflow-hidden opacity-75 hover:opacity-100 transition-opacity duration-200 disabled:opacity-75"
@@ -25,34 +28,67 @@ export function Login() {
             disabled={isLoggingIn}
           >
             <div className="bg-slate-50 dark:bg-slate-100">
-              <img className="w-8 h-8 ml-1" src={facebookLogo} alt="Facebook" />
+              {isLoggingIn ? (
+                <SpinnerGap
+                  size={32}
+                  weight="bold"
+                  className="ml-1 text-violet-600 animate-spin"
+                />
+              ) : (
+                <img
+                  className="w-8 h-8 ml-1"
+                  src={facebookLogo}
+                  alt="Facebook"
+                />
+              )}
             </div>
+
             <div className="flex items-center h-10 w-48 px-3 bg-blue-800">
               <span className="font-bold text-zinc-100">
                 Logar com Facebook
               </span>
             </div>
           </button>
+
           <button
             className="flex justify-start items-center gap-1 h-10 bg-slate-50 dark:bg-slate-200 rounded-[4px] overflow-hidden opacity-75 hover:opacity-100 transition-opacity duration-200 disabled:opacity-75"
             onClick={() => login(Provider.github)}
             disabled={isLoggingIn}
           >
             <div className="bg-slate-50 dark:bg-slate-100">
-              <img className="w-8 h-8 ml-1" src={githubLogo} alt="Facebook" />
+              {isLoggingIn ? (
+                <SpinnerGap
+                  size={32}
+                  weight="bold"
+                  className="ml-1 text-violet-600 animate-spin"
+                />
+              ) : (
+                <img className="w-8 h-8 ml-1" src={githubLogo} alt="Github" />
+              )}
             </div>
+
             <div className="flex items-center h-10 w-48 px-3 bg-neutral-800">
               <span className="font-bold text-zinc-100">Logar com Github</span>
             </div>
           </button>
+
           <button
             className="flex justify-start items-center gap-1 h-10 bg-slate-50 dark:bg-slate-200 rounded-[4px] overflow-hidden opacity-75 hover:opacity-100 transition-opacity duration-200 disabled:opacity-75"
             onClick={() => login(Provider.google)}
             disabled={isLoggingIn}
           >
             <div className="bg-slate-50 dark:bg-slate-100">
-              <img className="w-8 h-8 ml-1" src={googleLogo} alt="Facebook" />
+              {isLoggingIn ? (
+                <SpinnerGap
+                  size={32}
+                  weight="bold"
+                  className="ml-1 text-violet-600 animate-spin"
+                />
+              ) : (
+                <img className="w-8 h-8 ml-1" src={googleLogo} alt="Google" />
+              )}
             </div>
+
             <div className="flex items-center h-10 w-48 px-3 bg-red-800">
               <span className="font-bold text-zinc-100">Logar com Google</span>
             </div>
